fc741da94503710a61046fa3782943e9adb7959c
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-28 19:51:21 +0530

    loader-initial

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/help/Contact";
 import Faq from "./pages/help/Faq";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
-import NotFound from "./pages/NotFound";
+import CareersLayout from "./layouts/CareersLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +27,9 @@ const router = createBrowserRouter(
         <Route path="help" element={<HelpLayout />}>
           <Route path="faq" element={<Faq />} />
           <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="careers" element={<CareersLayout />}>
+          <Route index element={<Careers />} loader={careersLoader} />
         </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
