6bb5d708c69462a7dcbf42b4b993af6baa0e5243
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-28 18:30:16 +0530

    deeper_of_newermethod

---
+++ src/layouts/HelpLayout.js
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function HelpLayout() {
+  return (
+    <div className="help-layout">
+      <h2>Website Help</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi,
+        architecto?
+      </p>
+      <nav>
+        <NavLink to="faq">View the FAQ</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+}
