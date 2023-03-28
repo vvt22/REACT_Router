0c085cc3a835c6289c32d4593463a6be81ec3d96
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-28 08:18:47 +0530

    olderway

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return <div className="App">Hello, Engineers!</div>;
+  //this is older way of router
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          {/* navlink provides an extra class in anchor tag  */}
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
