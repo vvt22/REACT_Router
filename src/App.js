62a40b1ed654c548cfb0b07d803533f20a0427ed
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-28 09:05:16 +0530

    newmethod

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+// layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  //this is older way of router
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-          {/* navlink provides an extra class in anchor tag  */}
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
